ce63c04a66564c371a28bb3cb6253781a2fe0ea8
Zachary Hawkhead <zachary.hawkhead@durham.ac.uk>
2023-03-17 12:01:14 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name="castep2fs",
+setup(name="nc_cryst",
       version="1.0.0",
       packages=find_packages(),
-      description="CASTEP utility for calculating Fermi surfaces.",
-      url="https://github.com/zachary-hawk/castep2fs.git",
+      description="CASTEP utility for visualising crystals and displaying non-collinear magnetism.",
+      url="https://github.com/zachary-hawk/NC_CRYST.git",
       author="Zachary Hawkhead",
       author_email="zachary.hawkhead@durham.ac.uk",
       license="MIT",
@@ -18,7 +18,7 @@ setup(name="castep2fs",
                         "argparse",
                         "warnings",
                         "itertools"],
-      entry_points={"console_scripts":["castep2fs=Source.main:main",]
+      entry_points={"console_scripts":["nc_cryst=nc_cryst.main:main",]
       }
 
       )
